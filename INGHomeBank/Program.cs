8d5daeaed8dee0fa083e8c30ed066a325c0d55d3
mmiclea <madalin.miclea@tremend.com>
2017-09-06 17:48:45 +0300

    Add project files.

---
+++ INGHomeBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+namespace INGHomeBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.homebank.ro/public/HomeBankLogin/home";
+        }
+    }
+}
